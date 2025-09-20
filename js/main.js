619a8abc72cc8d5f04b57394a7a8aea22f38bf5a
Ray S Estrada <ray.s.est116@gmail.com>
2025-09-19 23:53:12 -0700

    Arreglé el botón, ahora inicio con el JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const productosArray = [
+    {
+        id: "abrigo-01",
+        titulo: "Abrigo 01",
+        imagen: "/img/abrigos/01.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    }
+]
